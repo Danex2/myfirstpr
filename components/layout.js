5030121e3a36d36df846a931be28353677c1ecc8
Danex2 <khadane.miller@gmail.com>
2020-05-17 17:20:12 -0400

    added github login

--- components/layout.js
+++ components/layout.js
@@ -1,42 +1,76 @@
 import Link from "next/link";
 import Head from "next/head";
+import { useAuthState } from "react-firebase-hooks/auth";
+import firebase from "../firebase/config";
 
-export default ({ children, title = "This is the default title" }) => (
-  <div className="flex flex-col min-h-screen" style={{ fontFamily: "Inter" }}>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&display=swap"
-        rel="stylesheet"
-      />
-    </Head>
-    <header>
-      <nav className="bg-gray-900 p-5 flex justify-around">
-        <h1 className="font-semibold text-white">My First PR</h1>
-        <div className="flex items-center text-blue-500">
-          {" "}
-          <Link href="/create">
-            <a>Create</a>
+export default ({ children, title = "This is the default title" }) => {
+  const { initialising, user } = useAuthState(firebase.auth());
+  const authenticate = () => {
+    const provider = new firebase.auth.GithubAuthProvider();
+    provider.addScope("repo");
+    firebase.auth().signInWithRedirect(provider);
+  };
+  console.log(user);
+  return (
+    <div className="flex flex-col min-h-screen" style={{ fontFamily: "Inter" }}>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <header>
+        <nav className="bg-gray-900 p-5 flex justify-around">
+          <Link href="/">
+            <a className="font-semibold text-white">My First PR</a>
           </Link>
-          <svg
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            className="h-5"
-          >
-            <path d="M9 5l7 7-7 7"></path>
-          </svg>
-        </div>
-      </nav>
-    </header>
+          {user ? (
+            <div className="flex items-center text-blue-500">
+              {" "}
+              <Link href="/create">
+                <a>Create</a>
+              </Link>
+              <svg
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                className="h-5"
+              >
+                <path d="M9 5l7 7-7 7"></path>
+              </svg>
+            </div>
+          ) : (
+            <div
+              className="flex items-center text-blue-500"
+              onClick={authenticate}
+            >
+              {" "}
+              <p>Login with Github</p>
+              <svg
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                className="h-5"
+              >
+                <path d="M9 5l7 7-7 7"></path>
+              </svg>
+            </div>
+          )}
+        </nav>
+      </header>
 
-    {children}
+      {children}
 
-    <footer className="bg-gray-900 p-3 text-white">Github link</footer>
-  </div>
-);
+      <footer className="bg-gray-900 p-3 text-white">Github link</footer>
+    </div>
+  );
+};
--- pages/create.js
+++ pages/create.js
@@ -3,7 +3,24 @@ import Layout from "../components/layout";
 export default function Create() {
   return (
     <Layout title="Create">
-      <div className="flex-grow bg-gray-200 ">test</div>
+      <div className="flex-grow bg-gray-200">
+        <form className="max-w-3xl m-auto grid grid-cols-2 mt-10">
+          <label class="block">
+            <span class="text-gray-700">Search</span>
+            <input
+              class="form-input mt-1 block w-full p-2 rounded border-2 border-gray-300"
+              placeholder="Search for github project"
+            />
+          </label>
+          <label class="block">
+            <span class="text-gray-700">Name</span>
+            <input
+              class="form-input mt-1 block w-full p-2 rounded border-2 border-gray-300"
+              placeholder="Search for github project"
+            />
+          </label>
+        </form>
+      </div>
     </Layout>
   );
 }
